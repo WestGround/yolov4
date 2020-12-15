7e0c2023a1fa5f7fdb79765b4773b92bd386b910
JHLEE <42939694+WestGround@users.noreply.github.com>
2020-12-16 00:21:34 +0900

    modify training images and save
    gray-scale, inversion, contrast, brightness, blur modifying to training image

---
+++ data/modify_image.py
@@ -0,0 +1,24 @@
+import os
+from PIL import Image, ImageEnhance, ImageFilter
+import matplotlib.pylab as plt
+
+os.chdir(os.path.join("data", "obj"))
+for fname in os.listdir(os.getcwd()):
+  if fname.endswith(".jpg"):
+    img = Image.open(fname)
+    for x in range(0,img.size[0]):
+      for y in range(0,img.size[1]):
+        rgb = img.getpixel((x,y))
+        rgb_wb = round((rgb[0]+rgb[1]+rgb[2])/3)
+        img.putpixel((x,y), (255-rgb_wb, 255-rgb_wb, 255-rgb_wb))
+    contrast = ImageEnhance.Contrast(img)
+    img = contrast.enhance(0.5)
+    bright = ImageEnhance.Brightness(img)
+    img = bright.enhance(0.9)
+    img = img.filter(ImageFilter.BoxBlur(5))
+    plt.imshow(img)
+    img.save(fname[:-4] + "-1.jpg")
+  elif fname.endswith(".txt"):
+    os.system("cp " + fname + " " + fname[:-4] + "-1.txt")
+
+os.chdir(os.path.join("..", ".."))
